310ffa79728e82538bea11b6875bd7d545e94772
MightyAcas <67846394+MightyAcas@users.noreply.github.com>
2024-04-02 00:14:35 -0400

    enter on combo

--- keyboards/voyager/keymaps/magic_vylet/keymap.c
+++ keyboards/voyager/keymaps/magic_vylet/keymap.c
@@ -16,21 +16,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     KC_TAB,         KC_W,           KC_C,           KC_M,           KC_P,           KC_B,                                           KC_X,           KC_L,           KC_O,           KC_U,           KC_J,           KC_MINUS,       
     KC_BSPC,        KC_R,           KC_S,           KC_T,           KC_H,           KC_F,                                           KC_Y,           KC_N,           KC_A,           KC_E,           KC_I,           KC_COMMA,       
-    OSM(MOD_LCTL),  KC_Q,           KC_V,           KC_G,           KC_D,           KC_K,                                           KC_Z,           US_AREP,        KC_DOT,         KC_QUOTE,       KC_SCLN,        KC_ENTER,       
+    OSM(MOD_LCTL),  KC_Q,           KC_V,           KC_G,           KC_D,           KC_K,                                           KC_Z,           US_AREP,        KC_DOT,         KC_QUOTE,       KC_SCLN,        KC_LEFT_GUI,       
                                                                                     KC_SPACE,       OSL(1),         RCTL(KC_BSPC),  OSM(MOD_RSFT)
   ),
   [1] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     KC_ESCAPE,      KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_SLASH,       KC_QUES,        KC_TILD,        
     KC_BSPC,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_GRAVE,       
-    KC_DELETE,      KC_MS_WH_DOWN,  KC_MS_WH_UP,    KC_MS_BTN2,     KC_MS_BTN1,     LLOCK,                                          KC_CAPS,        KC_EQUAL,       KC_PLUS,        KC_BSLS,        KC_PIPE,        KC_ENTER,       
+    KC_DELETE,      KC_MS_WH_UP,    KC_MS_WH_DOWN,  KC_MS_BTN2,     KC_MS_BTN1,     LLOCK,                                          KC_CAPS,        KC_EQUAL,       KC_PLUS,        KC_BSLS,        KC_PIPE,        KC_LEFT_GUI,       
                                                                                     KC_SPACE,       TO(0),          TO(3),          KC_DQUO
   ),
   [2] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     LED_LEVEL,      KC_HOME,        C(S(KC_T)),     KC_UP,          C(KC_W),        KC_PAGE_UP,                                     KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_MINUS,       
     KC_BSPC,        KC_END,         KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_PGDN,                                        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_COMMA,       
-    OSM(MOD_LCTL),  RGB_VAD,        RGB_VAI,        KC_MS_BTN2,     KC_MS_BTN1,     KC_PSCR,                                        KC_F11,         KC_F12,         KC_DOT,         KC_QUOTE,       KC_SCLN,        KC_ENTER,       
+    OSM(MOD_LCTL),  KC_MS_WH_UP,    KC_MS_WH_DOWN,  KC_MS_BTN2,     KC_MS_BTN1,     KC_PSCR,                                        KC_F11,         KC_F12,         KC_DOT,         RGB_VAD,        RGB_VAI,        KC_LEFT_GUI,       
                                                                                     KC_SPACE,       TO(0),          TO(3),          OSM(MOD_RSFT)
   ),
   [3] = LAYOUT_voyager(
@@ -46,16 +46,17 @@ const uint16_t PROGMEM comboLangle[] = { KC_W, KC_R, COMBO_END};
 const uint16_t PROGMEM comboLsquare[] = { KC_C, KC_S, COMBO_END};
 const uint16_t PROGMEM comboLcurly[] = { KC_M, KC_T, COMBO_END};
 const uint16_t PROGMEM comboLparen[] = { KC_P, KC_H, COMBO_END};
-const uint16_t PROGMEM comboOSMalt[] = { KC_B, KC_F, COMBO_END};
-const uint16_t PROGMEM comboOSMaltNAV[] = { KC_PAGE_UP, KC_PGDN, COMBO_END};
+const uint16_t PROGMEM comboEnter[] = { KC_H, KC_F, COMBO_END};
+const uint16_t PROGMEM comboEnterSYM[] = { KC_4, KC_5, COMBO_END};
+const uint16_t PROGMEM comboEnterNAV[] = { KC_RIGHT, KC_PGDN, COMBO_END};
+const uint16_t PROGMEM comboOSMalt[] = { KC_D, KC_K, COMBO_END};
+const uint16_t PROGMEM comboOSMaltSYMNAV[] = { KC_MS_BTN1, KC_PSCR, COMBO_END};
 
 const uint16_t PROGMEM comboRparen[] = { KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM comboRcurly[] = { KC_O, KC_A, COMBO_END};
 const uint16_t PROGMEM comboRsquare[] = { KC_U, KC_E, COMBO_END};
 const uint16_t PROGMEM comboRangle[] = { KC_J, KC_I, COMBO_END};
 
-const uint16_t PROGMEM comboGui[] = { KC_K, KC_SPACE, COMBO_END};
-const uint16_t PROGMEM comboGuiNAV[] = { KC_PSCR, KC_SPACE, COMBO_END};
 const uint16_t PROGMEM comboNav[] = { KC_SPACE, OSL(1), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
@@ -64,16 +65,17 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(comboLsquare, KC_LBRC),
     COMBO(comboLcurly, KC_LCBR),
     COMBO(comboLparen, KC_LPRN),
+    COMBO(comboEnter, KC_ENTER),
+    COMBO(comboEnterSYM, KC_ENTER),
+    COMBO(comboEnterNAV, KC_ENTER),
     COMBO(comboOSMalt, OSM(MOD_LALT)),
-    COMBO(comboOSMaltNAV, OSM(MOD_LALT)),
+    COMBO(comboOSMaltSYMNAV, OSM(MOD_LALT)),
 
     COMBO(comboRparen, KC_RPRN),
     COMBO(comboRcurly, KC_RCBR),
     COMBO(comboRsquare, KC_RBRC),
     COMBO(comboRangle, KC_RABK),
 
-    COMBO(comboGui, KC_LEFT_GUI),
-    COMBO(comboGuiNAV, KC_LEFT_GUI),
     COMBO(comboNav, TO(2)),
 };
 
